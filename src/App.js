554ca978081ba50ae42b310826c589b6942b7309
Venkatesh Rajendran <ven2701@gmail.com>
2020-02-24 00:08:12 +0530

    changed App to class based component && added state to it

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi.. I'm WinKey...</h1>
-      <Person name="Winkey" age="24">Let's rock buddy</Person>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person : [
+      {name : 'Winkey', age : 23}
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Hi.. I'm WinKey...</h1>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}>Let's rock buddy</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
