4bd2a5daf6c0b08ad2b98ec82e86cc20af65b69f
Venkatesh Rajendran <ven2701@gmail.com>
2020-02-24 00:37:57 +0530

    changed App.js to function based component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';//, { Component }
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    person : [
-      {name : 'Winkey', age : 23}
-    ]
-  }
-
-  switchNameHandler = () => {
-    console.log('Was clicked');
-    this.setState({person : [
-      {name : 'Venkatesh', age : 23},
-
-    ]});
-  }
-
-  render(){
+const app = ()=> {
+  
+  const person = [
+    {name : 'Winkey', age : 23}
+  ];
     return (
       <div className="App">
         <h1>Hi.. I'm WinKey...</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}>Let's rock buddy</Person>
+        <button >Switch Name</button>
+        <Person name={person[0].name} age={person[0].age}>Let's rock buddy</Person>
       </div>
     );
-  }
+  
 }
 
-export default App;
+export default app;
+
+// state = {
+//   person : [
+//     {name : 'Winkey', age : 23}
+//   ]
+// }
+
+// switchNameHandler = () => {
+//   console.log('Was clicked');
+//   this.setState({person : [
+//     {name : 'Venkatesh', age : 23},
+
+//   ]});
+// }
