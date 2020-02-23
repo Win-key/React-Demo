e21158e6ce211d01580b944be773f4b374ee7122
Venkatesh Rajendran <ven2701@gmail.com>
2020-02-24 00:55:28 +0530

    added useState

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React from 'react';//, { Component }
+import React,{useState} from 'react';//, { Component }
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
-const app = ()=> {
-  
-  const person = [
-    {name : 'Winkey', age : 23}
-  ];
+const App = props=> {
+  const [ personState, setPersonState] = useState({
+    person : [
+      {name : 'Winkey', age : 23}
+    ],
+    otherState : 'something else'
+  });  
+
+  const switchNameHandler = () => {
+    console.log('Was clicked');
+    setPersonState({person : [
+      {name : 'Venkatesh', age : 23} 
+    ]});
+  }
     return (
       <div className="App">
         <h1>Hi.. I'm WinKey...</h1>
-        <button >Switch Name</button>
-        <Person name={person[0].name} age={person[0].age}>Let's rock buddy</Person>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personState.person[0].name} age={personState.person[0].age}>Let's rock buddy</Person>
       </div>
     );
   
 }
 
-export default app;
-
-// state = {
-//   person : [
-//     {name : 'Winkey', age : 23}
-//   ]
-// }
-
-// switchNameHandler = () => {
-//   console.log('Was clicked');
-//   this.setState({person : [
-//     {name : 'Venkatesh', age : 23},
-
-//   ]});
-// }
+export default App;
