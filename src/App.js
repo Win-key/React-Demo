5f52de3a18d00fc607c07017124d61bb71e7c8d7
Venkatesh Rajendran <ven2701@gmail.com>
2020-02-26 00:38:49 +0530

    State/stateles, Parent to child passing function as param

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React,{useState} from 'react';//, { Component }
+import React,{Component} from 'react';//, { Component }
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
-const App = props=> {
-  const [ personState, setPersonState] = useState({
+class App extends Component {
+
+  state = {
+    person : [
+      {name : 'Winkey', age : 23}
+    ],
+    otherState : 'something else'
+  };
+  /*const [ personState, setPersonState] = useState({
     person : [
       {name : 'Winkey', age : 23}
     ],
@@ -13,25 +20,26 @@ const App = props=> {
 
   const [ otherState, setOtherState] = useState({
     otherState : 'something else'
-  });  
+  });*/  
 
-  const switchNameHandler = () => {
+  switchNameHandler = ([name, age]) => {
     console.log('Was clicked');
-    setPersonState({person : [
-      {name : 'Venkatesh', age : 23} 
+    //setPersonState
+    this.setState({person : [
+      {name : name, age : age} 
     ]});
-  }
+  };
 
-  console.log(personState, otherState);
-  
+  render(){
     return (
       <div className="App">
         <h1>Hi.. I'm WinKey...</h1>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personState.person[0].name} age={personState.person[0].age}>Let's rock buddy</Person>
+        <button onClick={this.switchNameHandler.bind(this, ['Venkatesh',24])}>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} click={this.switchNameHandler.bind(this, ['Venky',23])}>Let's rock buddy</Person>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} click={()=>this.switchNameHandler( ['React',4])}>Second Component</Person>
       </div>
     );
-  
+  };
 }
 
 export default App;
