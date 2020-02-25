052284aed54031cee7468e80dc3f194b3d7427a0
Venkatesh Rajendran <ven2701@gmail.com>
2020-02-26 00:55:36 +0530

    Two way binding

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
 
   state = {
     person : [
-      {name : 'Winkey', age : 23}
+      {name : 'Winkey', age : 23},
+      {name : 'Dynamic', age : 5}
     ],
     otherState : 'something else'
   };
@@ -26,9 +27,17 @@ class App extends Component {
     console.log('Was clicked');
     //setPersonState
     this.setState({person : [
-      {name : name, age : age} 
+      {name : name, age : age} ,
+      {name : 'React Learning', age : 5}
     ]});
   };
+
+  onChangeHandler = (event)=>{
+    this.setState({person : [
+      {name : 'Winkey', age : 23},
+      {name : event.target.value, age : 5}
+    ]});
+  }
 
   render(){
     return (
@@ -37,6 +46,8 @@ class App extends Component {
         <button onClick={this.switchNameHandler.bind(this, ['Venkatesh',24])}>Switch Name</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age} click={this.switchNameHandler.bind(this, ['Venky',23])}>Let's rock buddy</Person>
         <Person name={this.state.person[0].name} age={this.state.person[0].age} click={()=>this.switchNameHandler( ['React',4])}>Second Component</Person>
+        <input type='text' onChange={this.onChangeHandler}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age} click={()=>this.switchNameHandler( ['React',4])}>Two way bind Component</Person>
       </div>
     );
   };
